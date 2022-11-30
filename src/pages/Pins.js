7520b69c15707709663769652ef89963d22a8475
Georgi <figoto660@gmail.com>
2022-11-29 21:51:17 -0800

    add pin

--- src/pages/Pins.js
+++ src/pages/Pins.js
@@ -6,12 +6,29 @@ import PinDetails from "../components/PinDetails";
 import SearchBar from "../components/SearchBar";
 import Feed from "../components/Feed";
 
+const Pins = ({ user }) => {
+  const [search, setSearch] = useState("");
 
-const Pins = () => {
 
-  const [serach,setSeatch] =useState('');
-  
-  return <div>Pins</div>;
+
+  return (
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <NavBar search={search} setSearch={setSearch} user={user && user} />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route path="/pin-detail/:pinId" element={<PinDetails user={user && user} />} />
+          <Route path="/create-pin" element={<Pin user={user && user} />} />
+          <Route path="/search" element={<SearchBar search={search} setSearch={setSearch} />} />
+        </Routes>
+      </div>
+    </div>
+  );
+
+
 };
 
 export default Pins;
