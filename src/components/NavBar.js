cf5b3bf27f06c9fdbf481f455363e9dd464a7ae7
Georgi <figoto660@gmail.com>
2022-11-29 22:16:17 -0800

    create search bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiSearchAlt } from "react-icons/bi";
+import { RiAddFill } from "react-icons/ri";
 
+const NavBar = ({ user, search, setSearch }) => {
+  const navigate = useNavigate();
 
-const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    user && (
+      <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7">
+        <div className="flex justify-start items-center w-full px-2 rounded-lg bg-white border-none outline-none focus-within:shadow-sm">
+          <BiSearchAlt className="text-lg ml-1" />
+          <input
+          placeholder="Search..."
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            type="text"
+            className='p-2 w-full bg-white outline-none'
+            onFocus={() => navigate('/search')}
+          />
+        </div>
+      </div>
+    )
+  );
+};
 
-export default NavBar
+export default NavBar;
