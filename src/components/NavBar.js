a19893451cc9320da89a5cc65ec9dab6554f1c2d
Georgi <figoto660@gmail.com>
2022-11-29 22:37:16 -0800

    add to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,13 +12,28 @@ const NavBar = ({ user, search, setSearch }) => {
         <div className="flex justify-start items-center w-full px-2 rounded-lg bg-white border-none outline-none focus-within:shadow-sm">
           <BiSearchAlt className="text-lg ml-1" />
           <input
-          placeholder="Search..."
+            placeholder="Search..."
             onChange={(e) => setSearch(e.target.value)}
             value={search}
             type="text"
-            className='p-2 w-full bg-white outline-none'
-            onFocus={() => navigate('/search')}
+            className="p-2 w-full bg-white outline-none"
+            onFocus={() => navigate("/search")}
           />
+        </div>
+        <div className="flex gap-2">
+          <Link to={`user-profile/${user?._id}`} className="hidden md:block ">
+            <img
+              src={user.image}
+              alt="user-img"
+              className="w-12 h-12  rounded-lg "
+            />
+          </Link>
+          <Link
+            to="create-pin"
+            className="bg-black text-white text-lg rounded-lg h-12 w-12 flex justify-center items-center"
+          >
+            <RiAddFill />
+          </Link>
         </div>
       </div>
     )
