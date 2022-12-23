412c6b0bc11f6b4c17623375680d7ae3e19297af
Georgi <figoto660@gmail.com>
2022-12-22 21:28:33 -0700

    seatch bar finish

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,38 @@
 import { useEffect, useState } from "react";
 import { client } from "../client";
 import Layout from "./Layout";
-import { feedQuery,searchQuery } from "../data";
+import { feedQuery, searchQuery } from "../data";
 import Spinner from "./Spinner";
 
+const SearchBar = ({ search, setSearch }) => {
+  const [loading, setLoading] = useState(true);
+  const [pins, setPins] = useState("");
 
-const SearchBar = () => {
+  useEffect(() => {
+    if (search) {
+      setLoading(true);
+      const query = searchQuery(search.toLowerCase());
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+  }, [search]);
+
   return (
-    <div>SearchBar</div>
-  )
-}
+    <div>
+      {loading && <Spinner message="Please wait. Searching... " />}
+      {pins.length !== 0 && <Layout pins={pins} />}
+      {pins.length === 0 && search !== "" && !loading && (
+        <div className="mt-9 text-xl text-center"> No Photo Found</div>
+      )}
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
