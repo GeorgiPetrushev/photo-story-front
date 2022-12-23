f0d12570e193ba3929a635e6518323e3c7106ec1
Georgi <figoto660@gmail.com>
2022-12-22 22:55:17 -0700

    log in bug fix

--- src/App.js
+++ src/App.js
@@ -6,19 +6,26 @@ import Login from "./components/Login";
 const App = () => {
 
 
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const User = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+
+    if (!User) navigate('/login');
+  }, []);
 
 
   return (
-    <BrowserRouter>
+    
       <Routes>
         <Route path="login" element={<Login />} />
         <Route path="/*" element={<Home />} />
       </Routes>
 
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-        © 2022 . All Rights Reserved. Powered by Georgi Petrushev.
-      </span>
-    </BrowserRouter>
+      // <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      //   © 2022 . All Rights Reserved. Powered by Georgi Petrushev.
+      // </span>
+ 
   );
 };
 
