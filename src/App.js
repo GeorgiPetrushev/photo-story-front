9b2e314bfd2617497fe955894ae7aaf603f51992
Georgi <figoto660@gmail.com>
2022-12-22 22:46:28 -0700

    login bug fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./components/Login";
 
 const App = () => {
+
+
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
       </Routes>
 
       <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
